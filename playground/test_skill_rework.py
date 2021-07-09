3804eb221e0ff6735a23baac2556076fd028b5c4
Mirco Altenbernd <mirco.altenbernd@ians.uni-stuttgart.de>
2021-07-09 22:56:28 +0200

    cleanup

--- playground/test_skill_rework.py
+++
@@ -1,8 +0,0 @@
-import skill_rework as skill
-
-d = skill.Rive()
-d.setTalent(cadence = 1, flurry = 6, nothing = 1, sever = 3, twistingFangs = 3)
-
-for i in range(6):
-  damage, attacktime, durations = d.attack()
-  print(attacktime)
